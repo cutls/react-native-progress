dff77f6c3b15f34a259169095241e5e636f6ba74
cutls <web-pro@cutls.com>
2022-08-10 00:40:54 +0900

    type decoration

--- index.d.ts
+++ index.d.ts
@@ -1,4 +1,4 @@
-declare module 'react-native-progress' {
+declare module '@cutls/react-native-progress' {
   import React from 'react';
   import { TextStyle, ViewProperties, ColorValue } from 'react-native';
 
